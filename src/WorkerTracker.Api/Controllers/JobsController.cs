095caaf0cdf5ec70536ed0c1fbf5db57051f5daa
andriipetrenko85 <andriipetrenko85@gmail.com>
2020-11-24 01:49:15 +0200

    Add WorkerTracker.Api

---
+++ src/WorkerTracker.Api/Controllers/JobsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Abstractions.Services;
+using Domain.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebAPIWorkTracker.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JobsController
+    {
+        private readonly IJobService _jobService;
+        private readonly ILogger<JobsController> _logger;
+
+
+        public JobsController(IJobService jobService, ILogger<JobsController> logger)
+        {
+            _jobService = jobService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<StatusByRoomTypeViewModel>> GetProgressByRoomTypes()
+        {
+            _logger.LogDebug("GetProgressByRoomTypes");
+
+            return await _jobService.GetStatusesByRoomType();
+        }
+    }
+}
--- src/WorkerTracker.Api/Startup.cs
+++ src/WorkerTracker.Api/Startup.cs
@@ -3,12 +3,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using DataLayer;
-using Services;
 using Microsoft.EntityFrameworkCore;
+using Services;
+using DataLayer;
 
-namespace WebAPIWorkTracker
+namespace WorkerTracker.Api
 {
     public class Startup
     {
@@ -19,34 +18,25 @@ namespace WebAPIWorkTracker
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddDbContext<DataLayer.RXContext>(options =>
-            {                
-                options.UseSqlServer(Configuration.GetSection("ConnectionStrings:Default").Value);                
-            });            
-
-            services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIWorkTracker", Version = "v1" });
+                options.UseSqlServer(Configuration.GetSection("ConnectionStrings:Default").Value);
             });
+
             services.AddServiceLayer();
             services.AddDatalayer();
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIWorkTracker v1"));
-            }
-            else
-            {
-                app.UseExceptionHandler("/error");
             }
 
             app.UseHttpsRedirection();
